6d45ca1d268753d5a99d78ddac1de6621ff1fe40
Poompoomi <proulxlive@gmail.com>
2025-10-28 18:15:08 -0400

    Create 3sumclosest.py

---
+++ 3sumclosest.py
@@ -0,0 +1,53 @@
+import bisect
+
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        T = log(target**3, 10) # modulo operation, O(1)
+        closestvalue = 0
+        stack = []
+         #min operation, O(n), can be simplified to O(logn), To DO: Modulo 3 for each number
+
+        # nums.sort()
+        # sortednums = closest(nums, T/3)
+
+        closest = min(nums, key= lambda x:abs(x-T/3)) # O(n)
+        stack.append(closest)
+        
+        result = unwind_and_collect(stack)
+
+
+        if abs(result - target) == 0:
+            return closestvalue
+        if abs(result - target) < abs(closestvalue - target):
+            closestvalue = result
+
+        return closestvalue
+
+
+    def unwind_and_collect(stack, unwind_count=3):
+        cursor.append(stack.pop())
+
+        for _ in range(min(unwind_count-1, len(stack))):
+            cursor.append(stack[-1])
+        
+        return sum(cursor)
+
+    def closest(sorted_nums, target):
+        i = bisect.bisect_left(sorted_nums, target)
+        if i == 0: 
+        return sorted_nums[0]
+        if i == len(sorted_nums): 
+            return sorted_nums[-1]
+        before, after = sorted_nums[i-1], sorted_nums[i]
+        return before if abs(before - target) <= abs(after - target) else after
+
+        # put into stack structure to pop first 3 elements
+
+        #worm throwugh to find closest to target
+        # *** OPTIMIZATION: zeros in the array are FREE O(n) time OR LESS??
+
